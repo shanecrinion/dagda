2c8efe28fdb876644bbfbaaea26a881718143ba5
Shane Crinion <shanecrinion@gmail.com>
2025-06-29 14:38:59 +0100

    hard coded file loc

--- inst/app/app.R
+++ inst/app/app.R
@@ -4,7 +4,7 @@ library(dagda)
 library(tibble)
 library(stringr)
 
-test_data.clean <- readRDS(here::here('data', "test_data.rds"))
+test_data.clean <- readRDS(system.file('data', "test_data.rds"))
 
 generate_feedback_html <- function(word_row, correct = FALSE) {
   icon <- if (correct) "🎈" else "💀"
